f4589505866a9bb85d3615360fae986c0fd413b9
Juan Manuel Garcia <jmg.utn@gmail.com>
2014-03-05 15:59:06 -0300

    fix spaces

--- django_shopify/shopify_app/services/log_service.py
+++ django_shopify/shopify_app/services/log_service.py
@@ -4,15 +4,15 @@ from shopify_app.models import RequestLog
 
 class LogService(BaseService):
 
-	entity = RequestLog
+    entity = RequestLog
 
-	def log_request(self, request):
+    def log_request(self, request):
 
-		data = {
+        data = {
             "url": request.get_full_path(), 
             "params": str(request.REQUEST), 
             "headers": str(request.META), 
         }
 
-        log = self.new(**data)
+        log = self.new(**data)      
         log.save()
