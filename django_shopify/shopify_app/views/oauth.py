a587a296c4a3401c832b776bd54c96d2f0fafc30
Juan Manuel García <jmg.utn@gmail.com>
2014-04-07 15:33:57 -0300

    check if shops exists on finalizee callback

--- django_shopify/shopify_app/views/oauth.py
+++ django_shopify/shopify_app/views/oauth.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 from shopify_app.services.log_service import LogService
 from shopify_app.services.shopify_api import APIWrapper
+from shopify_app.services.shop_service import ShopService
 
 
 class BaseOauthView(BaseView):
@@ -30,6 +31,7 @@ class LoginView(BaseOauthView):
 
         #If the ${shop}.myshopify.com address is already provided in the URL, just skip to authenticate
         if self.request.REQUEST.get('shop'):
+
             shop = self.request.REQUEST.get('shop').strip()
             permission_url = APIWrapper(shop_url=shop).permissions_url()
 
@@ -55,12 +57,17 @@ class FinalizeView(BaseOauthView):
         if hasattr(self.request, 'session') and 'shopify' in self.request.session and self.request.session["shopify"]["shop_url"] == shop_url:
             return self.redirect(getattr(settings, "PREFERENCES_URL", DEFAULTS["PREFERENCES_URL"]))
 
-        # Initializing shopify session
-        try:
-            permanent_token = APIWrapper(shop_url=shop_url).permanent_token(self.request.GET["code"])
-        except:
-            #Shopify session fails, self.redirect to login initial step
-            return self.redirect("%s?shop=%s" % ("/oauth/login", shop_url))
+        shop = ShopService().get_one(myshopify_domain=shop_url)
+
+        if shop is None:
+            #If shop doesn't exists get the permanent token using the API
+            try:
+                permanent_token = APIWrapper(shop_url=shop_url).permanent_token(self.request.GET["code"])
+            except:
+                #Shopify session fails, self.redirect to login initial step
+                return self.redirect("%s?shop=%s" % ("/oauth/login", shop_url))
+        else:
+            permanent_token = shop.token
 
         # Store shopify sesssion data in our session
         self.request.session['shopify'] = {
